42de551934d2be01295a9b4c109683924283b459
Jakub Gańko <128095396+KubiMlodziutki@users.noreply.github.com>
2024-05-14 01:42:05 +0200

    Update config.py
    secure tokens

--- ListenersPrediction/config.py
+++ ListenersPrediction/config.py
@@ -1,4 +1,4 @@
 #config
-CLIENT_ID = "7be670b31a154fd3bd013e807118ebef"
-CLIENT_SECRET = "51c2cafc3f9544e8bd0fa24b5e2e76ee"
+CLIENT_ID = "xxx"
+CLIENT_SECRET = "xxx"
 SAVE_FILE = "spotify_data2.json"
